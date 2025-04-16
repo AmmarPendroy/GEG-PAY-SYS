68d20f1e5256e89b1726ca7053f21b14dcee2099
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-16 15:13:56 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -1,12 +1,6 @@
-import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, auth, db
+[firebase]
+database_url = "https://geg-pay-sys.firebaseio.com"
 
-# Load Admin SDK credentials
-cred = credentials.Certificate("geg-pay-sys-firebase-adminsdk-fbsvc-949f2a165b.json")
-
-# Initialize Firebase app once
-if not firebase_admin._apps:
-    firebase_admin.initialize_app(cred, {
-        "databaseURL": st.secrets["firebase"]["database_url"]
-    })
+[email]
+sender = "ammar.muhammed@geg-construction.com"
+password = "AmmarGEG99$"
