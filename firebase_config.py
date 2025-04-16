2ebb187e5061cf1bf266d047931fac0941f9b5bb
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-16 14:09:05 +0300

    Create firebase_config.py

---
+++ firebase_config.py
@@ -0,0 +1,15 @@
+import pyrebase
+import streamlit as st
+
+firebase_config = {
+    "apiKey": st.secrets["firebase"]["api_key"],
+    "authDomain": st.secrets["firebase"]["auth_domain"],
+    "projectId": st.secrets["firebase"]["project_id"],
+    "storageBucket": st.secrets["firebase"]["storage_bucket"],
+    "messagingSenderId": st.secrets["firebase"]["messaging_sender_id"],
+    "appId": st.secrets["firebase"]["app_id"],
+    "databaseURL": st.secrets["firebase"]["database_url"]
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+auth = firebase.auth()
