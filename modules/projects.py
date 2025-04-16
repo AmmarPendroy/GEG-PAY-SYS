e6784308365a4472c17ff4d38557dfa1181adae5
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-16 14:23:42 +0300

    Update projects.py

--- modules/projects.py
+++ modules/projects.py
@@ -5,7 +5,7 @@ PROJECTS_CSV = "data/projects.csv"
 
 def load_projects():
     if not os.path.exists(PROJECTS_CSV):
-        return pd.DataFrame(columns=["name"])
+        return pd.DataFrame(columns=["name", "location", "remark"])
     return pd.read_csv(PROJECTS_CSV)
 
 def save_projects(df):
