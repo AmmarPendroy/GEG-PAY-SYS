18bd514be1056274a22741042fbca315596be06e
AmmarPendroy <ammar.muhammed@geg-construction.com>
2025-04-17 11:16:49 +0300

    Update firebase_config.py

--- firebase_config.py
+++ firebase_config.py
@@ -2,10 +2,10 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, auth, db
 
-# Load Admin SDK credentials
+# Load service account JSON
 cred = credentials.Certificate("geg-pay-sys-firebase-adminsdk-fbsvc-949f2a165b.json")
 
-# Initialize Firebase app once
+# Initialize the Firebase Admin app
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred, {
         "databaseURL": st.secrets["firebase"]["database_url"]
